6a7e39406964cd91629588f1c3e7ce344bf1e62a
livemehere <rhdxoals@gmail.com>
2026-05-10 05:01:12 +0900

    feat: Segment display

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,35 @@
 #include <Arduino.h>
 
-constexpr int redPin = 27;
-constexpr int greenPin = 26;
-constexpr int bluePin = 25;
+const int DS = 27;
+const int STcp = 26;
+const int SHcp = 25;
 
-constexpr int freq = 5000;
-constexpr int resolution = 8;
-
-void setColor(int r, int g, int b){
-  ledcWrite(redPin, r);
-  ledcWrite(greenPin, g);
-  ledcWrite(bluePin, b);
-}
-
+int dataArr[] = {
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111, // 3
+  0b01100110, // 4
+  0b01101101, // 5
+  0b01111101, // 6
+  0b00000111, // 7
+  0b01111111, // 8
+  0b01101111, // 9
+};
 
 void setup() {
   Serial.begin(9600);
-  ledcAttach(redPin, freq, resolution);
-  ledcAttach(greenPin, freq, resolution);
-  ledcAttach(bluePin, freq, resolution);
+  pinMode(DS, OUTPUT);
+  pinMode(STcp, OUTPUT);
+  pinMode(SHcp, OUTPUT);
 }
 
 void loop() {
-  setColor(255, 0, 0);
-  delay(1000);
-  setColor(0, 255, 0);
-  delay(1000);
-  setColor(0, 0, 255);
-  delay(1000);
-  setColor(255, 255, 0);
-  delay(1000);
-  setColor(80, 0, 80);
-  delay(1000);
-  setColor(0, 255, 255);
-  delay(1000);
+ for(int i = 0; i < 10; i++){
+    digitalWrite(STcp, LOW);
+    shiftOut(DS, SHcp, MSBFIRST, dataArr[i]);
+    digitalWrite(STcp, HIGH);
+    delay(1000);
+    Serial.println(dataArr[i], BIN);
+ }
 }
