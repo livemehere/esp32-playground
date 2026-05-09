15002067ecd1b78072b82d7f38c8e91e423c1a25
livemehere <rhdxoals@gmail.com>
2026-05-10 01:30:07 +0900

    feat: colorful light

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <Arduino.h>
 
-constexpr int ledPin = 26;
-int brightness = 0;
-int fadeAmount = 5;
+constexpr int redPin = 27;
+constexpr int greenPin = 26;
+constexpr int bluePin = 25;
+
+constexpr int freq = 5000;
+constexpr int resolution = 8;
+
+void setColor(int r, int g, int b){
+  ledcWrite(redPin, r);
+  ledcWrite(greenPin, g);
+  ledcWrite(bluePin, b);
+}
+
 
 void setup() {
   Serial.begin(9600);
-  ledcAttach(ledPin, 5000, 8);
+  ledcAttach(redPin, freq, resolution);
+  ledcAttach(greenPin, freq, resolution);
+  ledcAttach(bluePin, freq, resolution);
 }
 
 void loop() {
-  ledcWrite(ledPin, brightness);
-  brightness += fadeAmount;
-  if(brightness <= 0 || brightness >= 255){
-    fadeAmount *= -1;
-  }
-  delay(50);
+  setColor(255, 0, 0);
+  delay(1000);
+  setColor(0, 255, 0);
+  delay(1000);
+  setColor(0, 0, 255);
+  delay(1000);
+  setColor(255, 255, 0);
+  delay(1000);
+  setColor(80, 0, 80);
+  delay(1000);
+  setColor(0, 255, 255);
+  delay(1000);
 }
